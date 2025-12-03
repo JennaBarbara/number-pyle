1881476f3c888677b200c4f0a6f726714dc3075b
jennabarbara <jennabarbara@gmail.com>
2025-12-03 15:52:08 -0500

    fix hydration error in console

--- src/components/game-mode-select.tsx
+++ src/components/game-mode-select.tsx
@@ -1,6 +1,5 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 
-import Button from './button'
 import { NavLink } from 'react-router'
 
 export default function GameModeSelect(){
@@ -8,7 +7,10 @@ export default function GameModeSelect(){
 
   return (
 <Menu>
-      <MenuButton className="focus-visible:outline-0"><Button size="sm">Game Mode</Button></MenuButton>
+      <MenuButton className="focus-visible:outline-0">
+        <div className="p-2 cursor-pointer outline-2 outline-black rounded-md bg-radial-[at_25%_25%] from-white via-stone-200 to-stone-100 hover:via-stone-100 hover:to-white disabled:opacity-50">
+          Game Mode
+        </div></MenuButton>
       <MenuItems anchor="bottom" className="mt-1 outline-0 border-2 rounded-md bg-stone-200 divide-solid divide-y-2 data-focus:outline-red">
         <MenuItem>
           <NavLink className="p-2 block data-focus:bg-white " to="/number-pyle">
