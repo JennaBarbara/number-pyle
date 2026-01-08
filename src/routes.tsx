4a367e2594ff091034f79db9e09247e4dfc4e392
jennabarbara <jennabarbara@gmail.com>
2026-01-08 17:03:29 -0500

    remove prefix from routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,9 @@
-import { type RouteConfig, index, route,prefix } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 
 export default [ 
-    ...prefix("number-pyle", [
+
     index("./modes/number-pyle/number-pyle.tsx"),
     route("number-pyre", "./modes/number-pyre/number-pyre.tsx"),
     route("number-scryer", "./modes/number-scryer/number-scryer.tsx"),
-    ])
 ] satisfies RouteConfig
