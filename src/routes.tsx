6178775b0d7ea2f53c126c193e9aa3a4a359284c
jennabarbara <jennabarbara@gmail.com>
2026-01-08 16:38:35 -0500

    working on routing

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,10 @@
-import { type RouteConfig, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, route,prefix } from "@react-router/dev/routes";
 
 
-export default [  
-    route("number-pyle", "./modes/number-pyle/number-pyle.tsx"),
-    route("number-pyle/number-pyre", "./modes/number-pyre/number-pyre.tsx"),
-    route("number-pyle/number-scryer", "./modes/number-scryer/number-scryer.tsx"),
+export default [ 
+    ...prefix("number-pyle", [
+    index("./modes/number-pyle/number-pyle.tsx"),
+    route("number-pyre", "./modes/number-pyre/number-pyre.tsx"),
+    route("number-scryer", "./modes/number-scryer/number-scryer.tsx"),
+    ])
 ] satisfies RouteConfig
